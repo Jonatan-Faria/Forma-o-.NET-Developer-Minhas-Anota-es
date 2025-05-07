96f717746482b6962c1c45edc8ccbe828a3a884e
Jonatan <jonatan.oliveiramdefaria@gmail.com>
2025-05-07 15:59:42 -0300

    Add files via upload

---
+++ Modulo 2/Propriedades, Métodos e Construtores com C#/Propriedades, métodos e construtores/models/Pessoa.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Humano.models
+{
+    public class Pessoa
+    {
+        /* ==========================================
+                       Propriedade
+          ===========================================*/
+        private string _nome;
+        public string Nome
+        {
+            get
+            {
+                return  _nome.ToUpper();
+            }
+
+            set
+            {
+                if (value == "" || value == " ")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+                _nome = value;
+            }
+        }
+        private string _sobrenome;
+
+        public string sobrenome
+        {
+            get
+            {
+                return _sobrenome.ToUpper();
+            }
+            set
+            {
+                if (value == "" || value == " ")
+                {
+                    throw new ArgumentException("O sobrenome não pode ser vazio");
+                }
+                _sobrenome = value;
+            }
+        }
+
+        public string nomeCompleto => $"{_nome} {_sobrenome}";
+
+        private int _idade;
+        public int idade
+        {
+            get
+            {
+                return _idade;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Sua idade não pode ser nula ou negativa");
+                }
+                idade = _idade;
+            }
+        }
+        /*============================================
+            Método = um codigo que pode ser utilizados varias vezes 
+          ============================================*/
+        public void apresentar()
+        {
+            Console.WriteLine($"Nome: {nomeCompleto} | Idade: {idade} ");
+        }
+
+
+    }
+}
